21fa7949d25f8cbf804199fcaea0f611bc61740e
darkground <blockads32@gmail.com>
2023-10-26 19:06:44 +0300

    (t1p4/shared): commit

---
+++ T1P4_TCP/T1P4_Shared/connector.cpp
@@ -0,0 +1,35 @@
+#include "connector.h"
+
+TConnector::TConnector(TConnParams& pars, QObject *parent) : QUdpSocket(parent)
+{
+    params = pars;
+    ready = bind(params.rHost, params.rPort,
+                 QAbstractSocket::ShareAddress | QAbstractSocket::ReuseAddressHint);
+    if (ready) connect(this,SIGNAL(readyRead()),this,SLOT(recieve()));
+}
+
+bool TConnector::isReady()
+{
+    return ready;
+}
+
+void TConnector::send(QByteArray msg)
+{
+    if (ready)
+    {
+        writeDatagram(msg, params.sHost, params.sPort);
+        qDebug()<<"SENT"<<msg;
+    }
+}
+
+void TConnector::recieve()
+{
+    if (hasPendingDatagrams())
+    {
+        quint64 size = pendingDatagramSize();
+        QByteArray msg(size,'\0');
+        readDatagram(msg.data(), size);
+        qDebug()<<"RECV"<<msg;
+        emit recieved(msg);
+    }
+}
